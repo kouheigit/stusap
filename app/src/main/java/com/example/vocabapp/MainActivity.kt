8b99b10b7ecff149b9aa66cb40c883061950bddc
kouheigit <ookawakouhei777@gmail.com>
2026-06-08 22:23:09 +0900

    Register bulk import screen

--- app/src/main/java/com/example/vocabapp/MainActivity.kt
+++ app/src/main/java/com/example/vocabapp/MainActivity.kt
@@ -64,6 +64,7 @@ private fun AppNav(navController: NavHostController = rememberNavController()) {
         composable(Route.StudyLog.path) { StudyLogScreen(navController) }
         composable(Route.Settings.path) { SettingsScreen(navController) }
         composable(Route.AddWord.path) { AddWordScreen(navController) }
+        composable(Route.BulkImport.path) { BulkImportScreen(navController) }
         composable(
             Route.WORD_IMPORT_PATTERN,
             arguments = listOf(navArgument("defaultType") { type = NavType.StringType; defaultValue = "word" })
